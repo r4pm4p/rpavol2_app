86f39eef4a6fc373a409b039b6e4b8366e217698
lsbrel <lucas.81.gabriel@gmail.com>
2025-01-13 12:56:47 -0300

    Configuração de maquina docker e tailwind css

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,6 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1 class="bg-slate-700">HOME</h1>
 </template>
