157c3a2f5b8b8c77120e7f3e346c19b071e83dbe
lsbrel <lucas.81.gabriel@gmail.com>
2025-01-16 15:39:18 -0300

    to deploy branch

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/rpavol2_app',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
