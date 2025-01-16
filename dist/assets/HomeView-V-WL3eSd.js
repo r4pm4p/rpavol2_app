f2ffb5304c90d5a256a436f67f1e3d374b25efbc
lsbrel <lucas.81.gabriel@gmail.com>
2025-01-16 15:17:52 -0300

    dist removido

--- dist/assets/HomeView-V-WL3eSd.js
+++
@@ -1,1 +0,0 @@
-import{d as t,r as o,c as n,t as r,u as s,o as a}from"./index-D5bJCSK_.js";const l=t({__name:"HomeView",setup(c){const e=o().request;return(u,p)=>(a(),n("p",null,"token: "+r(s(e).getToken()),1))}});export{l as default};
